239c0a8b82115c359441e9327df1271c04aaae34
Kevin Li <pi31415einstein@gmail.com>
2018-09-17 10:20:44 +1000

    fixed more bugs

--- TKhangman.py
+++ TKhangman.py
@@ -27,15 +27,23 @@ def startGame():
     return(goal)
 
 def TkGUI(position):
-    global inputField, variableString
+    global inputField, variableString, welcomeMessage, inputField, submitButton, secretWord, guessedLetters
     variableString = StringVar()
-    Label(root, text = 'Welcome to HANGMAN. Please guess the secret word.').grid(row = 1, columnspan = 5)
+    for dash in dashes:
+        secretWord = secretWord + dash
+    for letter in guessed:
+        guessedLetters = guessedLetters + letter + ' '
+    #Due to TKinter destroy not recognising widgets that are created and packed/"gridded" on the same line,
+    #I split the grid function to the next line for welcomeMessage and inputField.
+    welcomeMessage = Label(root, text = 'Welcome to HANGMAN. Please guess the secret word.')
+    welcomeMessage.grid(row = 1, columnspan = 5)
     Label(root, text = images[position]).grid(row = 3, columnspan = 5)
     Label(root, text = secretWord).grid(row = 5, columnspan = 5)
     Label(root, text = guessedLetters).grid(row = 7, columnspan = 5)
-    Button(root, text = 'Submit', command = checkGuess).grid(row = 9, column = 1)
-    inputField = Entry(root, text = 'Guessed Letter', textvariable = variableString).grid(row = 9, column = 2)
-    print('hi')
+    submitButton = Button(root, text = 'Submit', command = onSubmitCheckGuess).grid(row = 9, column = 1)
+    inputField = Entry(root, textvariable = variableString)
+    inputField.grid(row = 9, column = 2)
+    print(guessed)
 
 def listToString():
     for dash in dashes:
@@ -45,35 +53,42 @@ def listToString():
 
 def getGuess():
     guess = variableString.get()
-    return(guess)
+    return(guess)       
 
-def reGuess():
-    global inputField, variableString
-    root.destroy()
-    Label(root, text = 'You have already guessed that letter.').grid(row = 1, columnspan = 5)
-    Button(root, text = 'Submit', command = getGuess).grid(row = 3, column = 1)
-    inputField = Entry(root, text = 'Guessed Letter', textvariable = variableString).grid(row = 3, column = 2)
-    guess = variableString.get()
-    return(guess)
-
-def checkGuess():
-    global inputField, errors, correct
+def onSubmitCheckGuess():
+    global welcomeMessage, inputField, errors, correct
+    firstGuess = True
+    guess = getGuess()
     i = 0
     result = []
-    print('bye')
-    guess = variableString.get()
+    print(guessed)
+    print('.')
+    if guess in guessed:
+        firstGuess = False
+        welcomeMessage.destroy()
+        welcomeMessage = Label(root, text = 'You have already guessed that letter.')
+        welcomeMessage.grid(row = 1, columnspan = 5)
+        inputField.destroy()
+        inputField = Entry(root, textvariable = variableString)
+        inputField.grid(row = 9, column = 2)
+        return(True)
+    else:
+        welcomeMessage.destroy()
     if errors < 6:
-        while guess in guessed:
-            guess = reGuess()
         for letter in goal:
             if guess == letter:
                 if guess not in guessed:
-                    dashes[i] = guess + ' '
+                    dashes[i] = guess + ' '                    
+                    welcomeMessage = Label(root, text = 'Correct!')
+                    welcomeMessage.grid(row = 1, columnspan = 5)
                     correct = correct - 1
             i = i + 1
         if guess not in goal:
+            welcomeMessage = Label(root, text = 'Incorrect!')
+            welcomeMessage.grid(row = 1, columnspan = 5)
             errors = errors + 1
         guessed.append(guess)
+        print(dashes)
     result.append(correct)
     result.append(errors)
     return(result)
@@ -81,5 +96,4 @@ def checkGuess():
 if __name__ == '__main__':
     startGame()
     TkGUI(0)
-    checkGuess()
     root.mainloop()
